95c145db978473c7140e7f222d5dfee2d75ad36a
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2023-05-06 01:24:09 +0900

    一旦フロントは完成

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,103 +1,178 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+// import * as React from 'react';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
-import AlertButton from './Alertbutton';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import Item from '@mui/material/Grid';
+// import Item from '@mui/material/Grid';
 import AndroidIcon from '@mui/icons-material/Android';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import DateRangeIcon from '@mui/icons-material/DateRange';
+import AlertButton from './Alertbutton';
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Container from '@mui/material/Container';
 
 const TransactionList = () => {
+  // const [selectedItem, setSelectedItem] = React.useState(null); // 選択されたアイテムを保持するステート
+  // const [openModal, setOpenModal] = React.useState(false); // モーダルを開くかどうかを制御するステート
+
+  // const handleOpenModal = (item) => { // モーダルを開く
+  //   setSelectedItem(item);
+  //   setOpenModal(true);
+  // };
+
+  // const handleCloseModal = () => { // モーダルを閉じる
+  //   setOpenModal(false);
+  // };
+
   return (
-    <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
-    {itemData.map((item) => (
-      <div key={item.id}>
-        <ListItem
-          secondaryAction={
-            <AlertButton
-              title={item.status == 0 ? '非表示' : '再表示'}
-            />
-          }
-          disablePadding
+    <Container
+          sx={{
+            width: "100%",
+            textAlign: "center",
+            padding: "10px",
+          }}
         >
-          <ListItemButton>
-            <ListItemAvatar>
-              <Avatar alt='画像' src={`${item.img}`} />
-            </ListItemAvatar>
-            <Grid
-              container
-              spacing={3}
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              sx={{ width: '100%' }} // Grid全体の幅を100%にする
-            >
-              <Grid item xs={2}>
-                <Item>
-                  <ListItemText id={`${item.id}`} primary={`${item.name}`} />
-                </Item>
-              </Grid>
-              <Grid item xs={2}>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item>
-                    <DateRangeIcon />
-                  </Grid>
-                  <Grid item>
-                    <ListItemText primary={`${item.date}`} />
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs={2}>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item>
-                    <CurrencyExchangeIcon />
-                  </Grid>
-                  <Grid item>
-                    <ListItemText primary={`${item.points}`} />
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs={2}>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="flex-start"
-                  alignItems="center"
-                >
-                  <Grid item>
-                    <AndroidIcon />
-                  </Grid>
-                  <Grid item>
-                    <ListItemText primary={`${item.owner}`} />
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </ListItemButton>
-        </ListItem>
-        <Divider variant="inset" component="li" />
-      </div>
-    ))}
-  </List>
-  
+    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+       {itemData.map((item) => (
+      <div>
+      <ListItem key={item.id}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <ListItemAvatar>
+            <Avatar alt='画像' src={`${item.img}`} />
+          </ListItemAvatar>
+        </Grid>
+        <Grid item xs={3}>
+          <ListItemText primary={item.name} secondary={item.date} />
+        </Grid>
+        <Grid item xs={1}>
+          <ListItemText primary={<CurrencyExchangeIcon />} secondary={item.points} />
+        </Grid>
+        <Grid item xs={5}>
+          <ListItemText primary={<AndroidIcon />} secondary={item.owner} />
+        </Grid>
+        <Grid item>
+        <Button variant='contained' sx={{right: '0'}}>詳細</Button>
+        </Grid>
+        <Grid item xs sx={{textAlign: 'right'}}>
+        <AlertButton title={item.status == 0 ? '非表示' : '再表示'} />
+        </Grid>
+      </Grid>
+    </ListItem>
+    <Divider variant="inset" component="li" />
+    </div>
+       ))}
+    </List>
+    </Container>
+//     <div>
+//       <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
+//   {itemData.map((item) => (
+//     <div key={item.id}>
+//       <ListItem disablePadding>
+//         <Grid
+//           container
+//           direction="row"
+//           justifyContent="space-between"
+//           alignItems="center"
+//           sx={{ width: '100%' }}
+//         >
+//           <Grid item sx={{ width: '80%' }}>
+//               <ListItemAvatar>
+//                 <Avatar alt='画像' src={`${item.img}`} />
+//               </ListItemAvatar>
+//               <Grid
+//                 container
+//                 spacing={3}
+//                 direction="row"
+//                 justifyContent="center"
+//                 alignItems="center"
+//               >
+//                 <Grid item xs={2}>
+//                   <Item>
+//                     <ListItemText id={`${item.id}`} primary={`${item.name}`} />
+//                   </Item>
+//                 </Grid>
+//                 <Grid item xs={2}>
+//                   <Grid
+//                     container
+//                     direction="row"
+//                     justifyContent="flex-start"
+//                     alignItems="center"
+//                   >
+//                     <Grid item>
+//                       <DateRangeIcon />
+//                     </Grid>
+//                     <Grid item>
+//                       <ListItemText primary={`${item.date}`} />
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+//                 <Grid item xs={2}>
+//                   <Grid
+//                     container
+//                     direction="row"
+//                     justifyContent="flex-start"
+//                     alignItems="center"
+//                   >
+//                     <Grid item>
+//                       <CurrencyExchangeIcon />
+//                     </Grid>
+//                     <Grid item>
+//                       <ListItemText primary={`${item.points}`} />
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+//                 <Grid item xs={2}>
+//                   <Grid
+//                     container
+//                     direction="row"
+//                     justifyContent="flex-start"
+//                     alignItems="center"
+//                   >
+//                     <Grid item>
+//                       <AndroidIcon />
+//                     </Grid>
+//                     <Grid item>
+//                       <ListItemText primary={`${item.owner}`} />
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+//               </Grid>
+//           </Grid>
+//           <Grid item>
+//             <Grid container direction="row" alignItems="center">
+//               <Grid item>
+//                 <Button>詳細</Button>
+//               </Grid>
+//               <Grid item>
+//                 <AlertButton title={item.status == 0 ? '非表示' : '再表示'} />
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </ListItem>
+//       <Divider variant="inset" component="li" />
+//     </div>
+//   ))}
+// </List>
+
+//       </div>
   );
 }
+
 export default TransactionList;
 
 const itemData = [
