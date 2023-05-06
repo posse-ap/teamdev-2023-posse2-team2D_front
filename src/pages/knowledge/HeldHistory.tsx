ce6ccb7de2f26edb0af27eb6e0c0e63160706660
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2023-05-06 13:08:27 +0900

    ナレッジ共有のフロント完成

--- src/pages/knowledge/HeldHistory.tsx
+++ src/pages/knowledge/HeldHistory.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
-import Container from '@mui/material/Container';
+import SimpleTabs from "../../components/SimpleTabs";
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import AndroidIcon from "@mui/icons-material/Android";
+import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
+import AlertButton from "../../components/Alertbutton";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import Container from "@mui/material/Container";
 
 const LentHistory = () => {
+  const HeldData = [
+    {
+      id: 1,
+      name: "ハッカソンしませんか？",
+      member: 5,
+      points: 100,
+      date: "2021/10/01",
+      status: 0,
+    },
+    {
+      id: 1,
+      name: "React勉強会",
+      member: 8,
+      points: 100,
+      date: "2021/10/01",
+      status: 1,
+    },
+  ];
+
   return (
     <div>
       <Container
@@ -9,8 +39,67 @@ const LentHistory = () => {
           textAlign: "right",
           padding: "10px",
         }}
-        >
-      <h2>月末獲得ポイント:{'値埋め込み'}</h2>
+      >
+        <h2>月末獲得ポイント:{"値埋め込み"}</h2>
+      </Container>
+      <Container
+        sx={{
+          width: "100%",
+          textAlign: "center",
+          padding: "10px",
+        }}
+      >
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          {HeldData.map((knowledge) => (
+            <div>
+              <ListItem key={knowledge.id}>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={3}>
+                    <ListItemText
+                      primary={knowledge.name}
+                      secondary={knowledge.date}
+                    />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <ListItemText
+                      primary={<PeopleAltIcon />}
+                      secondary={`${knowledge.member}人`}
+                    />
+                  </Grid>
+                  <Grid item xs={5}>
+                    <ListItemText
+                      primary={<CurrencyExchangeIcon />}
+                      secondary={`合計${knowledge.points}`}
+                    />
+                  </Grid>
+                  <Grid item xs={3} sx={{ textAlign: "right" }}>
+                    <Grid container spacing={1}>
+                      <Grid item xs={6}>
+                        <Button variant="contained" sx={{ right: "0" }}>
+                          開催報告
+                        </Button>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <AlertButton
+                          title={
+                            knowledge.status == 0 ? "開催停止" : "開催再開"
+                          }
+                          message={`${
+                            knowledge.status == 0 ? "開催を停止" : "開催を再開"
+                          }しますか？`}
+                          variant={`${
+                            knowledge.status == 0 ? "contained" : "outlined"
+                          }`}
+                        />
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </div>
+          ))}
+        </List>
       </Container>
     </div>
   );
--- src/pages/knowledge/ParticipationHistory.tsx
+++ src/pages/knowledge/ParticipationHistory.tsx
@@ -1,7 +1,58 @@
 import SimpleTabs from "../../components/SimpleTabs";
-import Container from '@mui/material/Container';
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import AndroidIcon from "@mui/icons-material/Android";
+import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Container from "@mui/material/Container";
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
 const ParticipationHistory = () => {
+
+  const ReservedData = [
+    {
+      id: 1,
+      name: "ハッカソンしませんか？",
+      points: 100,
+      date: "2021/10/01",
+      owner: "なおき",
+      status: 0,
+    },
+    {
+      id: 1,
+      name: "SaaS勉強会",
+      points: 100,
+      date: "2021/10/01",
+      owner: "なおき",
+      status: 0,
+    }
+  ];
+
+  const ParticipatedData = [
+    {
+      id: 1,
+      name: "htmlから頑張りましょう",
+      points: 100,
+      date: "2021/10/01",
+      owner: "なおき",
+      status: 0,
+    },
+    {
+      id: 1,
+      name: "AWS勉強会",
+      points: 100,
+      date: "2021/10/01",
+      owner: "なおき",
+      status: 0,
+    }
+  ];
+
   return (
     <div>
       <Container
@@ -9,14 +60,100 @@ const ParticipationHistory = () => {
           textAlign: "right",
           padding: "10px",
         }}
-        >
-      <h2>今月の消費ポイント:{'値埋め込み'}</h2>
+      >
+        <h2>今月の消費ポイント:{"値埋め込み"}</h2>
       </Container>
       <SimpleTabs
         labels={["予約中", "参加済"]}
         children={[
-          <div></div>,
-          <div></div>,
+          <div>
+            <Container
+              sx={{
+                width: "100%",
+                textAlign: "center",
+                padding: "10px",
+              }}
+            >
+              <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+                {ReservedData.map((knowledge) => (
+                  <div>
+                    <ListItem key={knowledge.id}>
+                      <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={3}>
+                          <ListItemText
+                            primary={knowledge.name}
+                            secondary={knowledge.date}
+                          />
+                        </Grid>
+                        <Grid item xs={1}>
+                          <ListItemText
+                            primary={<AndroidIcon />}
+                            secondary={knowledge.owner}
+                          />
+                        </Grid>
+                        <Grid item xs={5}>
+                          <ListItemText
+                            primary={<CurrencyExchangeIcon />}
+                            secondary={knowledge.points}
+                          />
+                        </Grid>
+                        <Grid item xs={3} sx={{ textAlign: "right" }}>
+                          <Button variant="contained" sx={{ right: "0" }}>
+                            詳細
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </div>
+                ))}
+              </List>
+            </Container>
+          </div>,
+          <div>
+            <Container
+              sx={{
+                width: "100%",
+                textAlign: "center",
+                padding: "10px",
+              }}
+            >
+              <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+                {ParticipatedData.map((knowledge) => (
+                  <div>
+                    <ListItem key={knowledge.id}>
+                      <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={3}>
+                          <ListItemText
+                            primary={knowledge.name}
+                            secondary={knowledge.date}
+                          />
+                        </Grid>
+                        <Grid item xs={1}>
+                          <ListItemText
+                            primary={<AndroidIcon />}
+                            secondary={knowledge.owner}
+                          />
+                        </Grid>
+                        <Grid item xs={5}>
+                          <ListItemText
+                            primary={<CurrencyExchangeIcon />}
+                            secondary={knowledge.points}
+                          />
+                        </Grid>
+                        <Grid item xs={3} sx={{ textAlign: "right" }}>
+                          <Button variant="contained" sx={{ right: "0" }}>
+                            感謝を伝える
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </div>
+                ))}
+              </List>
+            </Container>
+          </div>,
         ]}
       />
     </div>
